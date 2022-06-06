24733d04db4c744e8119906fcdd56c382218fb5c
CookieStudioCanada <92235638+CookieStudioCanada@users.noreply.github.com>
2022-06-06 00:33:21 -0400

    V2
    Cap update : Roster change auto. Still have to manually adress the cap.

--- freeagents.js
+++ freeagents.js
@@ -1,21 +1,51 @@
 // free agents
 
 document.getElementById("lebronJames").onclick = function(){
-    teamARoster[1] = lebronJamesID;
+
+    if(teamASalaryCap < 10000000){
+        teamARoster[1] = lebronJamesID;
+        console.log("You add L.James to your roster.")
+        updateRoster();
+    } else console.log("You don't have the cap space.")
+    
 }
 
 document.getElementById("stephCurry").onclick = function(){
-    teamARoster[4] = stephCurryID;
+
+    if(teamASalaryCap < 10000000){
+        teamARoster[4] = stephCurryID;
+        console.log("You add S.Curry to your roster.")
+        updateRoster();
+    } else console.log("You don't have the cap space.")
+
 }
 
 document.getElementById("nikolaJokic").onclick = function(){
-    teamARoster[0] = nikolaJokicID;
+
+    if(teamASalaryCap < 10000000){
+        teamARoster[0] = nikolaJokicID;
+        console.log("You add N.Jokic to your roster.")
+        updateRoster();
+    } else console.log("You don't have the cap space.")
+
 }
 
 document.getElementById("giannisAntetokounmpo").onclick = function(){
-    teamARoster[2] = giannisAntetokounmpoID;
+
+    if(teamASalaryCap < 10000000){
+        teamARoster[2] = giannisAntetokounmpoID;
+        console.log("You add G.Antetokounmpo to your roster.")
+        updateRoster();
+    } else console.log("You don't have the cap space.")
+        
 }
 
 document.getElementById("jamesHarden").onclick = function(){
-    teamARoster[3] = jamesHardenID;
+
+    if(teamASalaryCap < 10000000){
+        teamARoster[3] = jamesHardenID;
+        console.log("You add J.Harden to your roster.")
+        updateRoster();
+    } else console.log("You don't have the cap space.")
+        
 }
